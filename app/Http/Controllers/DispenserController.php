72c02a27674e68522ee380083869638d0d02a5b6
rn-santos227 <rn_santos227@yahoo.com>
2020-12-13 16:47:51 +0800

    add files

--- app/Http/Controllers/DispenserController.php
+++ app/Http/Controllers/DispenserController.php
@@ -63,16 +63,14 @@ class DispenserController extends Controller
         $user = Auth::user();
         $id = $request->id;
 
-        if(Hash::make($request->password) == $user->password) {
-            $dispenser = Dispenser::findOrFail($request->id);
-            $cap = $dispenser->capacity;
-            if($cap > 0) {
-                shell_exec("python /home/pi/test-relay".$request->id.".py 2>&1");
-                $dispenser->update([
-                    'capacity' => $cap - 1,
-                    'answered' => 1
-                ]);
-            }
+        $dispenser = Dispenser::findOrFail($request->id);
+        $quantity = $dispenser->quantity;
+        if($quantity > 0) {
+            shell_exec("python /home/pi/test-relay".$id.".py 2>&1");
+            $dispenser->update([
+                'quantity' => $quantity - 1,
+                'answered' => 1
+            ]);
         }
 
         return response()->json([
