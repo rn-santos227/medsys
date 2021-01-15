8cee87a9bcbef1d17d6d35714dd75ec75812e5c7
rn-santos227 <rn_santos227@yahoo.com>
2021-01-16 03:00:36 +0800

    add item

--- app/Http/Controllers/NurseController.php
+++ app/Http/Controllers/NurseController.php
@@ -53,7 +53,7 @@ class NurseController extends Controller
     }
 
     public function biometric(NurseRequest $request) {
-        exec("python /usr/share/doc/python-fingerprint/examples/example_enroll.py 2>&1");
+        shell_exec("python /usr/share/doc/python-fingerprint/examples/example_enroll.py 2>&1");
         $nurses = Nurse::where('active', 1)->get();
         return view('nurses.index', compact(
             'nurses'
