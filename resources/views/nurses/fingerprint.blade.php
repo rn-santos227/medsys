ce10b6da5a2adac467980132b896323365d9e2b5
rn-santos227 <rn_santos227@yahoo.com>
2021-01-16 03:54:22 +0800

    add item

--- resources/views/nurses/fingerprint.blade.php
+++ resources/views/nurses/fingerprint.blade.php
@@ -1,6 +1,6 @@
 @foreach ($nurses as $nurse)
-<div class="modal fade bd-example-modal-lg" id="fingerprint_{{ $nurse->id }}"  role="dialog" tabindex="-1" aria-labelledby="fingerprint_modal" aria-hidden="true">
-    <div class="modal-dialog modal-lg " role="document">
+<div class="modal fade bd-example-modal-sm" id="fingerprint_{{ $nurse->id }}"  role="dialog" tabindex="-1" aria-labelledby="fingerprint_modal" aria-hidden="true">
+    <div class="modal-dialog modal-sm " role="document">
         <form action="/nurses/fingerprint" method="GET">
             <div class="modal-content">
                 <div class="modal-header">
@@ -9,10 +9,16 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
+                <div class="modal-body">
                 @csrf
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="txt_first_name">Register Fingerprint of the Nurse.</label>
+                        </div>
+                    </div>
+                </div>
                 <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Okay</button>
                 </div>
             </form>
         </div>
